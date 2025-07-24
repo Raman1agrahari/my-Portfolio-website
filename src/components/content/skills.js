29269e62718a9756e9cf1a808d46632591682d7a
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-07-25 01:06:26 +0530

    new

---
+++ src/components/content/skills.js
@@ -0,0 +1,153 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiGit,
+  SiTypescript,
+  SiReact,
+  SiC,
+  SiCplusplus,
+  SiSupabase,
+  SiNodedotjs,
+  SiRedux,
+  SiMongodb,
+  SiGithub,
+  SiExpress,
+  SiGraphql,
+  SiMysql,
+  SiDocker,
+  SiJenkins,
+  SiAmazonaws,
+  SiRedis,
+  SiNginx,
+  SiKubernetes,
+  SiApachekafka,
+  SiAngular,
+  SiSocketdotio,
+  SiPostgresql,
+  SiBootstrap,
+} from "react-icons/si";
+
+const skills = [
+  {
+    name: "HTML",
+    Icon: SiHtml5,
+  },
+  {
+    name: "CSS",
+    Icon: SiCss3,
+  },
+  {
+    name: "Javascript",
+    Icon: SiJavascript,
+  },
+  {
+    name: "Typescript",
+    Icon: SiTypescript,
+  },
+  {
+    name: "React.js",
+    Icon: SiReact,
+  },
+  {
+    name: "Redux",
+    Icon: SiRedux,
+  },
+  {
+    name: "Next.js",
+    Icon: SiNextdotjs,
+  },
+  {
+    name: "Tailwind CSS",
+    Icon: SiTailwindcss,
+  },
+  {
+    name: "Bootstrap",
+    Icon: SiBootstrap,
+  },
+  {
+    name: "Node.js",
+    Icon: SiNodedotjs,
+  },
+  {
+    name: "ExpressJs",
+    Icon: SiExpress,
+  },
+  {
+    name: "GraphQL",
+    Icon: SiGraphql,
+  },
+  {
+    name: "MongoDB",
+    Icon: SiMongodb,
+  },
+  {
+    name: "MySql",
+    Icon: SiMysql,
+  },
+  {
+    name: "PostgreSQL",
+    Icon: SiPostgresql,
+  },
+  {
+    name: "Socket.io",
+    Icon: SiSocketdotio,
+  },
+  {
+    name: "Git",
+    Icon: SiGit,
+  },
+  {
+    name: "Github",
+    Icon: SiGithub,
+  },
+  {
+    name: "Docker",
+    Icon: SiDocker,
+  },
+  {
+    name: "Kubernetes",
+    Icon: SiKubernetes,
+  },
+  ,
+  {
+    name: "Jenkins",
+    Icon: SiJenkins,
+  },
+  {
+    name: "Aws",
+    Icon: SiAmazonaws,
+  },
+  {
+    name: "Redis",
+    Icon: SiRedis,
+  },
+  {
+    name: "Nginx",
+    Icon: SiNginx,
+  },
+  {
+    name: "Kafka",
+    Icon: SiApachekafka,
+  },
+  {
+    name: "C",
+    Icon: SiC,
+  },
+  {
+    name: "C++",
+    Icon: SiCplusplus,
+  },
+  {
+    name: "Angular",
+    Icon: SiAngular,
+  },
+  {
+    name: "React Native",
+    Icon: SiReact,
+  },
+];
+
+export default skills;
