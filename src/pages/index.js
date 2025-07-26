b6af110eae63b23ca32c006a33c5c8fbeed7f1fe
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-07-27 03:33:05 +0530

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
       <Transition />
       <main className="flex font-mont items-center text-dark w-full dark:text-light min-h-screen">
         <Layout className="pt-0 md:!pt-16 sm:!pt-8 mb-[8rem]">
-          <div className="flex items-center justify-between w-full lg:flex-col gap-8 ">
-            <div className="w-1/2 md:w-full flex justify-center items-center translate-x-[1px] md:translate-x-0">
+          <div className="flex items-center justify-between w-full lg:flex-col ">
+            <div className="w-1/2 md:w-full">
               <Image
                 src={profilePic}
                 alt="Image"
@@ -47,7 +47,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="https://drive.google.com/file/d/1FQuTTq_yDkuTolmRimZM66vkQUFK692g/view?usp=drivesdk"
+                  href="https://drive.google.com/file/d/1qeQ91o3ZYfzdADYADAEZckEgVbpNlhNF/view?usp=sharing"
                   target={"_blank"}
                   download={true}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark  border-solid border-transparent hover:border-dark border-2 dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md-px-4 md:text-base ">
