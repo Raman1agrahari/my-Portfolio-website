b33bcc770833cbdec38c83b503f1ade69814d401
Raman1agrahari <115079147+Raman1agrahari@users.noreply.github.com>
2025-09-01 21:52:14 +0530

    add

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -69,7 +69,7 @@ const Experience = () => {
         <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           {/* Amandy Labs Private Limited Experience */}
           <Details
-            position="Mern Stack Developer (Onsite Training)"
+            position="Mern Stack Developer (Onsite Internship)"
             company="Amandy Labs Private Limited"
             time="May 2025 - Present"
             address="Kanpur"
